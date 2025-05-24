c95099d3268c1e9020bc58c5c2e16bdcc716bcca
micmc422 <micmc422@gmail.com>
2025-05-24 17:27:35 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,11 @@ import mdx from "@next/mdx";
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
-  options: {},
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  trailingSlash: true,
   transpilePackages: ["next-mdx-remote"],
   sassOptions: {
     compiler: "modern",
