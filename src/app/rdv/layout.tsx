f573e75bab375749d3bb28655905f8895c88da94
micmc422 <micmc422@gmail.com>
2025-06-13 22:56:41 +0200

    ajout page rdv

---
+++ src/app/rdv/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'Prendre rendez-vous - Occitaweb',
+    description: 'Prenez rendez-vous avec Occitaweb pour discuter de vos projets web.',
+}
+export default function RdvLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            {children}
+        </>
+    );
+}
---
+++ src/app/rdv/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import Script from 'next/script'
+import { use, useEffect } from 'react'
+
+declare global {
+    interface Window {
+        dataLayer: any[];
+    }
+}
+
+
+export default function RdvPage() {
+    let dataLayer = []
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            window.dataLayer = window.dataLayer || [];
+            function gtag(p0: string, p1: any){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', 'G-0FWXELBBJL');
+        }
+    }, []);
+    return (
+        <>
+            <Script strategy="lazyOnload"  async src="https://www.googletagmanager.com/gtag/js?id=G-0FWXELBBJL">
+            </Script>
+        </>
+    )
+}
