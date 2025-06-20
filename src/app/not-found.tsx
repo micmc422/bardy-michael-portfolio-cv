f01bda6c6e62c93246b4af0de7a3117f72592a6d
micmc422 <micmc422@gmail.com>
2025-06-20 10:19:46 +0200

    Améliore la page 404 avec un message en français et ajoute un bouton de retour à l'accueil

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,38 @@
-import { Column, Heading, Text } from "@/once-ui/components";
+import { Column, Flex, Heading, Icon, IconButton, Text } from "@/once-ui/components";
 
 export default function NotFound() {
   return (
-    <Column as="section" fill center paddingBottom="160">
+    <Column as="section" fill center paddingBottom="40" gap="l">
       <Text marginBottom="s" variant="display-strong-xl">
         404
       </Text>
       <Heading marginBottom="l" variant="display-default-xs">
-        Page Not Found
+      😥 Page introuvable
       </Heading>
-      <Text onBackground="neutral-weak">The page you are looking for does not exist.</Text>
+      <Flex
+        fitWidth
+        border="brand-alpha-medium"
+        style={{
+          backdropFilter: "blur(var(--static-space-1))",
+        }}
+        background="brand-alpha-weak"
+        radius="full"
+        marginTop="20"
+        padding="4"
+        gap="8"
+        marginBottom="m"
+        vertical="center"
+      >
+        <Icon paddingLeft="12" name="home" onBackground="brand-weak" />
+        <Flex paddingX="8">Retourner à l'accueil</Flex>
+        <IconButton
+          href={"/"}
+          data-border="rounded"
+          variant="secondary"
+          icon="chevronRight"
+        />
+      </Flex>
+
     </Column>
   );
 }
