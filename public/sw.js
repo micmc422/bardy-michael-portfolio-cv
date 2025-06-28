f5c06f5dc9f98aa406053a89e1075669abb1b708
micmc422 <micmc422@gmail.com>
2025-06-28 09:50:43 +0200

    fix: corriger le traitement des données de notification push pour gérer les erreurs de parsing JSON

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,19 @@
 
 self?.addEventListener('push', function (event) {
     if (event.data) {
-        const data = event.data.json()
+        let data;
+        try {
+            data = event.data.json(); // Essayer de parser comme JSON
+        } catch (e) {
+            // Si ce n'est pas du JSON, essayez de le lire comme du texte brut
+            console.warn('Push data is not JSON, treating as plain text.');
+            data = {
+                title: 'Notification', // Titre par défaut
+                body: event.data.text(), // Le contenu est le texte brut
+                icon: '/images/apple-icon-180x180.png', // Icône par défaut
+            };
+        }
+
         const options = {
             body: data.body,
             icon: data.icon || '/images/apple-icon-180x180.png',
@@ -13,7 +25,7 @@ self?.addEventListener('push', function (event) {
                 primaryKey: '2',
             },
         }
-        event.waitUntil(self.registration.showNotification(data, options))
+        event.waitUntil(self.registration.showNotification(data.title, options)) // Utiliser data.title ici
     }
 })
 
